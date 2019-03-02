15cae5eb01b866320c9dcd48db27b28dc27c9404
vtretyak <tretyak1c@gmail.com>
2019-03-02 07:30:19 +0200

    commit

--- FivePages/Startup.cs
+++ FivePages/Startup.cs
@@ -6,12 +6,19 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace FivePages
 {
 	public class Startup
 	{
+		public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
+
+		public IConfiguration Configuration { get; }
 		// This method gets called by the runtime. Use this method to add services to the container.
 		// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
 		public void ConfigureServices(IServiceCollection services)
@@ -48,7 +55,7 @@ namespace FivePages
 			{
 				routes.MapRoute(
 					name: "default",
-					template: "{controller=Home}/{action=Index}/{id?}"
+					template: "{controller=Home}/{action=Index}"
 					);
 			});
 		}
